fb58f632fc08a314953fcb4110cda017ef5e9f19
shashiRgowda <shashikumartr64@gmail.com>
2021-04-14 09:40:31 +0530

    Added:arthmeticsorting.sh

---
+++ arthmeticsorting.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+echo "Enter given input:"
+read a
+echo "Enter given input:"
+read b
+echo "Enter given input:"
+read c
+echo "a+b*c value is $(($a+$b*$c))"
+echo "a*b+c value is $(($a*$b+$c))"
+echo "a+b*c value is $(($a+$b*$c))"
+echo "c+a*b value is $(($c+$a*$b))"
+echo "a+b*c value is $(($a+$b*$c))"
+echo "a%b+c value is $(($a%$b+$c))"
+echo "completed succesfully"
+
